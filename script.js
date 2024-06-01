531a1fd1719a622adc7644a6d9f63c7bda739295
nino <ninosopoian@gmail.com>
2024-06-01 21:57:45 +0400

    add burger menu functional

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+
+// mobile-menu 
+
+const mobileMenu = document.getElementById("mobile-menu");
+const navMenu = document.querySelector("nav ul");
+
+mobileMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  mobileMenu.classList.toggle("rotate");
+});
