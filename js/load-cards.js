0614976883eb6a125f04ffa77bf8fcbe9b2609c1
nino <ninosopoian@gmail.com>
2024-06-04 17:00:55 +0400

    add cards styles

--- js/load-cards.js
+++ js/load-cards.js
@@ -1,14 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   const coursesData = [
-    { title: "iOS Development", date: "24 ივნისს", imgSrc: "card1.jpg" },
-    { title: "Android Development", date: "25 ივნისს", imgSrc: "card2.jpg" },
-    { title: "Web Development", date: "26 ივნისს", imgSrc: "card3.jpg" },
-    { title: "Data Science", date: "27 ივნისს", imgSrc: "card4.jpg" },
-    { title: "Machine Learning", date: "28 ივნისს", imgSrc: "card5.jpg" },
-    { title: "Cyber Security", date: "29 ივნისს", imgSrc: "card6.jpg" },
-    { title: "Cloud Computing", date: "30 ივნისს", imgSrc: "card7.jpg" },
-    { title: "UI/UX Design", date: "1 ივლისს", imgSrc: "card8.jpg" },
-    { title: "Blockchain", date: "2 ივლისს", imgSrc: "card9.jpg" },
+    {
+      title: "iOS Development",
+      date: "გაიხსნება 24 ივნისს",
+      imgSrc: "card1.jpg",
+    },
+    { title: "React", date: "გაიხსნება 24 ივნისს", imgSrc: "card2.jpg" },
+    {
+      title: "intro to Python",
+      date: "გაიხსნება 24 ივნისს",
+      imgSrc: "card3.jpg",
+    },
+    {
+      title: "Advanced Python",
+      date: "გაიხსნება 24 ივნისს",
+      imgSrc: "card4.jpg",
+    },
+    { title: "Advanced Cybersecurity", date: "10 ივნისს", imgSrc: "card5.jpg" },
+    { title: "Blockchain", date: "გაიხსნება 10 ივნისს", imgSrc: "card6.jpg" },
+    { title: "DevOps", date: "გაიხსნება 30 ივნისს", imgSrc: "card7.jpg" },
+    {
+      title: "Information Security Govermance",
+      date: "გაიხსნება 10 ივნისს",
+      imgSrc: "card8.jpg",
+    },
+    {
+      title: "ToT - Training of Trainers",
+      date: "დასრულებულია",
+      imgSrc: "card9.jpg",
+    },
   ];
 
   const cardsContainer = document.getElementById("cards-container");
@@ -21,15 +41,15 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="card__img">
                 <img src="./images/${course.imgSrc}" alt="${course.title}">
             </div>
-            <div class="card__content">
+            
                 <div class="card__title">
                     <h3>${course.title}</h3>
-                    <span>რეგისტრაცია გაიხსნება ${course.date}</span>
+                    <span>რეგისტრაცია ${course.date}</span>
                 </div>
                 <div class="card__btn">
                     <a class="card__link" href="#">კურსის დეტალები</a>
                 </div>
-            </div>
+            
         `;
 
     cardsContainer.appendChild(card);
